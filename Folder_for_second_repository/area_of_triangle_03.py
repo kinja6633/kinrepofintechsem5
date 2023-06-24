6fff0292cacdad87bc6f9807c928a3d3ca2e6c11
unknown <kinjaaa6633@gmail.com>
2023-06-23 23:16:36 -0700

    this is file in a first repo

--- Folder_for_second_repository/area_of_triangle_03.py
+++
@@ -1,6 +0,0 @@
-# Python program to find the area of a triangle
-base = input("Enter base:")
-height = input("Enter Height:")
-
-area = (int(base) * int(height) / 2)
-print(area)
--- Folder_for_second_repository/odd_even_011.py
+++
@@ -1,7 +0,0 @@
-# Python Program to Check if a Number is Odd or Even
-num = int(input("Enter any number:"))
-
-if num%2==0:
-    print("IT'S EVEN")
-else:
-    print("IT'S ODD")
